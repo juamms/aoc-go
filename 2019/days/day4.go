3b5d765a249409cef5acbe9757bd8de0a92e2483
Joao Silva <joao.ms94@gmail.com>
2021-02-24 01:23:34 -0100

    [2019] Add day 4 solutions.

--- 2019/days/day4.go
+++ 2019/days/day4.go
@@ -1,11 +1,93 @@
 package days
 
+import (
+	"github.com/juamms/aoc-go/2019/utils"
+)
+
 type Day4 struct{}
 
+func hasDoubles(pw int) bool {
+	prev := -1
+
+	for pw != 0 {
+		n := pw % 10
+		pw = pw / 10
+
+		if prev == n {
+			return true
+		}
+
+		prev = n
+	}
+
+	return false
+}
+
+func doesNotDecrease(pw int) bool {
+	prev := 9
+
+	for pw != 0 {
+		n := pw % 10
+		pw = pw / 10
+
+		if n > prev {
+			return false
+		}
+
+		prev = n
+	}
+
+	return true
+}
+
+func hasValidDoubles(pw int) bool {
+	doubles := make(map[int]int)
+
+	for pw != 0 {
+		n := pw % 10
+		pw = pw / 10
+
+		if _, ok := doubles[n]; ok {
+			doubles[n]++
+		} else {
+			doubles[n] = 1
+		}
+	}
+
+	for _, v := range doubles {
+		if v == 2 {
+			return true
+		}
+	}
+
+	return false
+}
+
 func (day Day4) Part1() interface{} {
-	return 0
+	passwordRange, err := utils.GetInputIntSlice(4, "-")
+	utils.Handle(err)
+
+	validPasswords := 0
+	for i := passwordRange[0]; i <= passwordRange[1]; i++ {
+		if hasDoubles(i) && doesNotDecrease(i) {
+			validPasswords++
+		}
+
+	}
+
+	return validPasswords
 }
 
 func (day Day4) Part2() interface{} {
-	return 0
+	passwordRange, err := utils.GetInputIntSlice(4, "-")
+	utils.Handle(err)
+
+	validPasswords := 0
+	for i := passwordRange[0]; i <= passwordRange[1]; i++ {
+		if doesNotDecrease(i) && hasValidDoubles(i) {
+			validPasswords++
+		}
+	}
+
+	return validPasswords
 }
